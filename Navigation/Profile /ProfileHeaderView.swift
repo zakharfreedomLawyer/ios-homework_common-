d488f405fb37619bf66771622692f830f2c8753b
zakharfreedomLawyer <107808581+zakharfreedomLawyer@users.noreply.github.com>
2022-11-27 21:45:01 +0300

    Домашняя работа выполнена_попытка №2

--- Navigation/Profile /ProfileHeaderView.swift
+++ Navigation/Profile /ProfileHeaderView.swift
@@ -9,24 +9,18 @@ import UIKit
 
 
 class ProfileHeaderView: UIView {
-    
-   let lightGrayView = UIView()
-    
-
+        
     override init(frame: CGRect) {
         super.init(frame: frame)
-    
-        addSubview(lightGrayView) 
-    
         self.backgroundColor  = .lightGray
         setUpUI()   
 }
     
+    
     //MARK: Создаем кнопку ShowStatus
     
     private let blueeButton: UIButton = {
-    let buttonShowStatus = UIButton(frame: CGRect(x: 32, y: 250, width: 300, height: 50))
-        
+        let buttonShowStatus = UIButton(frame: CGRect(x: 16, y: 300, width: 355,  height: 50))
         buttonShowStatus.setTitle("Show status", for: .normal)
         buttonShowStatus.titleLabel?.textColor = .white
         buttonShowStatus.titleLabel?.font = UIFont.systemFont(ofSize: 18)
@@ -37,12 +31,12 @@ class ProfileHeaderView: UIView {
         buttonShowStatus.layer.shadowRadius = 4
         buttonShowStatus.layer.shadowColor = UIColor.black.cgColor
         buttonShowStatus.translatesAutoresizingMaskIntoConstraints = false
-        
         buttonShowStatus.setTitle("I am pressed", for: .highlighted)
         buttonShowStatus.addTarget(self, action: #selector(buttonPressed(sender:)), for: .touchUpInside)
+        
         return buttonShowStatus
     } ()
-    
+     
     @objc func buttonPressed (sender:UIButton) {
      let userText = textFieldShowStatus.text
         print(userText ?? ("nil"))
@@ -51,13 +45,13 @@ class ProfileHeaderView: UIView {
     //MARK: Создаем аватарку
      
      private lazy var catAvatarImageView: UIImageView = {
-         let imageView = UIImageView(frame: CGRect(x: 16, y: 100, width: 135, height: 135))
+         let imageView = UIImageView(frame: CGRect(x: 16, y: 110, width: 135, height: 135))
          imageView.image = UIImage(named: "catMaria")
          imageView.layer.borderWidth = 3
          imageView.layer.borderColor = UIColor(ciColor: .white).cgColor
          imageView.layer.cornerRadius = imageView.frame.size.width / 2
          imageView.clipsToBounds = true
-         imageView.translatesAutoresizingMaskIntoConstraints = false
+         //imageView.translatesAutoresizingMaskIntoConstraints = false
          return imageView
      }()
     
@@ -70,75 +64,80 @@ class ProfileHeaderView: UIView {
         textField.placeholder = "I am waiting eat..."
         textField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         textField.backgroundColor = .lightGray
-        textField.textColor = UIColor.gray
         textField.translatesAutoresizingMaskIntoConstraints = false
-        
+        textField.textColor = UIColor.gray
+    
         return textField
     }()
     
     //MARK: Создаем лейбл
      
      private var labelNameCat: UILabel = {
-         let labelName = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+         let labelName = UILabel(frame: CGRect(x: 200, y: 150, width: 50, height: 50))
          labelName.text = "Scottish shorthair"
          labelName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
          labelName.translatesAutoresizingMaskIntoConstraints = false
-         
          return labelName
      }()
+    
+    
+    //MARK: Дополнительное View для Navigation Bar
+    
+    private var whiteView: UIView {
+        let whiteViewBar = UIView()
+        whiteViewBar.backgroundColor = .white
+        whiteViewBar.frame = CGRect(x: 0, y: 0, width: 400, height: 47)
+        
+        return whiteViewBar
+    }
      
     //MARK: Функция,которая отвечает за добавление элементов
     
     func setUpUI() {
 
-     addSubview(blueeButton)
-     addSubview(catAvatarImageView)
+    addSubview(blueeButton)
+    addSubview(catAvatarImageView)
     addSubview (textFieldShowStatus)
     addSubview (labelNameCat)
-          
-   //MARK: Constrains
+    addSubview(whiteView)
+         
         
-        // Constrains for image
         
-        NSLayoutConstraint.activate([
-  
-            catAvatarImageView.leftAnchor.constraint(equalTo: lightGrayView.leftAnchor, constant: 16),
-            catAvatarImageView.topAnchor.constraint(equalTo: lightGrayView.topAnchor, constant: 16),
-           catAvatarImageView.topAnchor.constraint(equalTo: catAvatarImageView.bottomAnchor, constant: 16),
-           catAvatarImageView.bottomAnchor.constraint(equalTo: blueeButton.topAnchor, constant: -16)
-          ])
-          
-           // Constrains for button
+    //MARK: Constraints
+        
+        //Constraint for button
         
         NSLayoutConstraint.activate([
-           
-            blueeButton.leadingAnchor.constraint(equalTo: lightGrayView.leadingAnchor, constant: 16),
-            blueeButton.rightAnchor.constraint(equalTo: lightGrayView.rightAnchor, constant: -16),
-           blueeButton.topAnchor.constraint(equalTo: catAvatarImageView.bottomAnchor, constant: 16),
-           blueeButton.heightAnchor.constraint(equalTo: blueeButton.heightAnchor, constant: 50),
-           blueeButton.topAnchor.constraint(equalTo: textFieldShowStatus.bottomAnchor, constant: 34)
-        ])  
-        
-        // Constrains for textfield
+            blueeButton.topAnchor.constraint(equalTo: catAvatarImageView.bottomAnchor, constant: 16),
+            blueeButton.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 16),
+            blueeButton.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -16)
+        ])
+
+        //Constraint for avatar
         
         NSLayoutConstraint.activate([
-            textFieldShowStatus.trailingAnchor.constraint(equalTo: lightGrayView.trailingAnchor, constant: -60),
-           textFieldShowStatus.leftAnchor.constraint(equalTo: catAvatarImageView.rightAnchor, constant: 16)
+            catAvatarImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: -16),
+            catAvatarImageView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 16),
+    
         ])
         
-        // Constrains for label
-       NSLayoutConstraint.activate([ 
-           labelNameCat.bottomAnchor.constraint(equalTo: textFieldShowStatus.topAnchor, constant: -20),
-          labelNameCat.leftAnchor.constraint(equalTo: catAvatarImageView.rightAnchor, constant: 16),
-           labelNameCat.trailingAnchor.constraint(equalTo: labelNameCat.trailingAnchor, constant: 27),
-       ])
+        //Constraint for label
+        
+        NSLayoutConstraint.activate([
+            labelNameCat.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
+            labelNameCat.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 158)
+        ])
+        
+        //Constraint for textfield
+        
+        NSLayoutConstraint.activate([
+            textFieldShowStatus.bottomAnchor.constraint(equalTo: blueeButton.topAnchor, constant: -34),
+            textFieldShowStatus.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 160)
+        ])
         
     }
-
- 
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-} 
- 
+}
--- Navigation/Profile /ProfileViewController.swift
+++ Navigation/Profile /ProfileViewController.swift
@@ -13,19 +13,19 @@ import UIKit
     
    var profileHeaderView = ProfileHeaderView()
    
-   
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         profileHeaderView.backgroundColor = .lightGray
         view.addSubview(profileHeaderView)
         self.title = "Profile"
-        self.navigationController?.navigationBar.backgroundColor = .white 
+        self.navigationController?.navigationBar.backgroundColor = .white
     
-    } 
+   }
+     
       
     override func viewDidLayoutSubviews() {
-        profileHeaderView.frame = view.frame
-  
+        profileHeaderView.frame = view.frame 
     }
 }
+
